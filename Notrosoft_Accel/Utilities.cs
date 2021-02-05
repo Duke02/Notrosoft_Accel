92ac71eb5403b4eb76968f55f5edb8e473deb8ad
Trystan May <trystanmay@gmail.com>
2021-02-05 13:15:21 -0800

    Added docs to utilities.

--- Notrosoft_Accel/Utilities.cs
+++ Notrosoft_Accel/Utilities.cs
@@ -5,8 +5,17 @@ using System.Text;
 
 namespace Notrosoft_Accel
 {
+    /// <summary>
+    /// General utility functions to use throughout the project.
+    /// </summary>
     public static class Utilities
     {
+        /// <summary>
+        /// Flattens the provided 2D container into a 1D container.
+        /// </summary>
+        /// <typeparam name="T">The type of data within the container.</typeparam>
+        /// <param name="values">The values to flatten.</param>
+        /// <returns>The input flattened into a 1D container.</returns>
         public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> values)
         {
             return values.SelectMany(vals => vals);
