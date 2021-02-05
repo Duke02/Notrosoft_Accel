ba3da5e8f4bdffaa5bf76fc14b40a33564471473
Trystan May <trystanmay@gmail.com>
2021-02-05 12:43:34 -0800

    Added Median file and added tests.

--- Notrosoft_Accel.Tests/StatisticsTest.cs
+++ Notrosoft_Accel.Tests/StatisticsTest.cs
@@ -49,6 +49,63 @@ namespace Notrosoft_Accel.Tests
             Assert.AreEqual(expected, actual, _tolerance, $"Mode Statistic does not give the correct value!");
         }
 
+        [TestMethod]
+        public void Statistics_Mean_Test()
+        {
+            var meanStatistic = new MeanStatistic();
+            var data = new List<List<double>>();
+            for (int i = 0; i < 2; i++)
+            {
+                data.Add(new List<double>
+                {
+                    1, 0, 1, 1, 0
+                });
+            }
 
+            var expected = 6.0 / 10.0;
+            var actual = meanStatistic.Operate(data);
+
+            Assert.AreEqual(expected, actual, _tolerance, $"Mean Statistic does not give the correct value!");
+        }
+
+        [TestMethod]
+        public void Statistics_Median_TestOdd()
+        {
+            var medianStatistic = new MedianStatistic();
+            var data = new List<List<double>>();
+            for (int i = 0; i < 3; i++)
+            {
+                data.Add(new List<double>
+                {
+                    1, 1, 2, 3, 4
+                });
+            }
+            // 1X, 1X, 1X, 1X, 1X, 1X, 2X, 2, 2X, 3X, 3X, 3X, 4X, 4X, 4X
+
+            var expected = 2.0;
+            var actual = medianStatistic.Operate(data);
+
+            Assert.AreEqual(expected, actual, _tolerance);
+        }
+
+        [TestMethod]
+        public void Statistics_Median_TestEven()
+        {
+            var medianStatistic = new MedianStatistic();
+            var data = new List<List<double>>();
+            for (int i = 0; i < 2; i++)
+            {
+                data.Add(new List<double>
+                {
+                    1, 2, 2, 3, 4, 5
+                });
+            }
+            // 1X, 1X, 2X, 2X, 2X, 2, 3, 3X, 4X, 4X, 5X, 5X
+
+            var expected = 2.5;
+            var actual = medianStatistic.Operate(data);
+
+            Assert.AreEqual(expected, actual, _tolerance);
+        }
     }
 }
