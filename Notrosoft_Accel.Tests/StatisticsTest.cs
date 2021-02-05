28e6f2108a586f1f9bf1e00926378cab72695335
Trystan May <trystanmay@gmail.com>
2021-02-05 12:05:04 -0800

    Added test project.

---
+++ Notrosoft_Accel.Tests/StatisticsTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Notrosoft_Accel.Tests
+{
+    [TestClass]
+    public class StatisticsTest
+    {
+        [TestMethod]
+        public void Statistics_Mode_Test()
+        {
+
+        }
+    }
+}
