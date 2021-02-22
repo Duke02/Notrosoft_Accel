a10b3a41d31edb81cdfdec293d4459c8cca50e10
Trystan May <trystanmay@gmail.com>
2021-02-22 09:22:35 -0800

    Added different data types.

---
+++ Notrosoft_Accel/Data/FrequencyData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notrosoft_Accel.Data
+{
+    /// <summary>
+    /// Frequency Data is data that counts the number of times an event occurs.
+    /// </summary>
+    /// <remarks>
+    /// TODO: Ask Delugach if we need to keep track of which event that is? And just general clarification on what he's expecting for this data.
+    /// </remarks>
+    class FrequencyData : GenericData<int>
+    {
+        public FrequencyData(IEnumerable<IEnumerable<int>> data) : base(data, isFrequency: true)
+        {
+        }
+    }
+}
---
+++ Notrosoft_Accel/Data/GenericData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Notrosoft_Accel.Data
+{
+    public class GenericData<T>
+    {
+        public IEnumerable<IEnumerable<T>> Data { get; protected set; }
+
+        public bool IsOrdinal { get; protected set; }
+
+        public bool IsFrequency { get; protected set; }
+
+        public bool IsInterval { get; protected set; }
+
+        protected GenericData(IEnumerable<IEnumerable<T>> data, bool isOrdinal = false, bool isFrequency = false, bool isInterval = false)
+        {
+            Data = data;
+            IsOrdinal = isOrdinal;
+            IsFrequency = isFrequency;
+            IsInterval = isInterval;
+        }
+
+        public T GetData(int row, int col)
+        {
+            return Data.ElementAt(row).ElementAt(col);
+        }
+
+        public T this[int row, int col]
+        {
+            get => GetData(row, col);
+        }
+    }
+}
