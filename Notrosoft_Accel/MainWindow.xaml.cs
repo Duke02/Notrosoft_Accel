b7df453a1e91289e21f82cfc0c7d18737b96b3a4
CDFain <57308827+CDFain@users.noreply.github.com>
2021-02-22 19:46:00 -0600

    GUI updates; DataGrid binding to DataTable complete

--- Notrosoft_Accel/MainWindow.xaml.cs
+++ Notrosoft_Accel/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace Notrosoft_Accel
 {
@@ -20,10 +21,54 @@ namespace Notrosoft_Accel
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static int colNum = 10;
         public MainWindow()
         {
+            List<List<double>> arr2d = new List<List<double>>();
+            List<double> rows = new List<double>();
+            rows.Add(12.1);
+            rows.Add(13.1992);
+            arr2d.Add(rows);
+            arr2d.Add(rows);
+
             InitializeComponent();
             Console.WriteLine("Hello World!");
+        }
+        void dataGridTable()
+        {
+            DataTable notData = new DataTable();
+
+            char name = 'a';
+            String nS = ("" + name);
+            for (int i = 0; i < colNum; i++)
+            {
+                DataColumn c1 = new DataColumn(nS, typeof(double));
+                name++;
+                nS = ("" + name);
+                notData.Columns.Add(c1);
+            }
+            
+            DataRow r1 = notData.NewRow();
+            
+            r1[0] = 12.1;
+            r1[1] = 13.1992;
+            r1[5] = 0.01;
+            r1[6] = 101;
+
+            DataRow r2 = notData.NewRow();
+            r2[0] = 1313.3131;
+            r2[1] = 0.922388811;
+
+            notData.Rows.Add(r1);
+            notData.Rows.Add(r2);
+
+            
+            Data.ItemsSource = notData.DefaultView;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dataGridTable();
         }
     }
 }
