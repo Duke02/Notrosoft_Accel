9e7efbc844bc205b23f4ec74a64a4338870e3b21
CDFain <57308827+CDFain@users.noreply.github.com>
2021-03-18 09:49:08 -0500

    Comments added during the presentation lmao.

--- Notrosoft_Accel/MainWindow.xaml.cs
+++ Notrosoft_Accel/MainWindow.xaml.cs
@@ -11,10 +11,10 @@ namespace Notrosoft_Accel
     /// </summary>
     public partial class MainWindow : Window
     {
-        public static int colNum = 10;  // Number of Columns present in the data structure
-        public static int rowNum = 10;  // Number of Rows present in the data structure
-        public static List<List<string>> dataList = new List<List<string>>();   // The data structure
-        public static DataTable dataTable = new DataTable();  // DataTable sits between the dataList of the backend and DataGrid of the GUI
+        public static int colNum = 10;  // Number of Columns present in the data structure.
+        public static int rowNum = 10;  // Number of Rows present in the data structure.
+        public static List<List<string>> dataList = new List<List<string>>();   // The data structure.
+        public static DataTable dataTable = new DataTable();  // DataTable sits between the dataList of the backend and DataGrid of the GUI.
 
         public MainWindow()
         {
@@ -25,7 +25,7 @@ namespace Notrosoft_Accel
             {
                 colConstruct.Add("");
             }
-            // Construct the rows as the empty column structure
+            // Construct the rows as the empty column structure.
             for (int i = 0; i < rowNum; i++)
             {
                 dataList.Add(colConstruct);
@@ -38,19 +38,21 @@ namespace Notrosoft_Accel
         // Constructs a dataTable object and binds it to the DataGrid's ItemsSource.
         void dataGridTable()
         {
-            // Set the dataTable to a new object
+            // Set the dataTable to a new object.
             dataTable = new DataTable();
 
-            // Column header constructor
-            char currentNameCharacter = 'A';                // The character that determines the name of the column
-            int iteration = 1;                              // The current iteration through the uppercase letters 
-            String newColName = ("" + currentNameCharacter);// The constructed first name (should simply by "A")
+            // Column header constructor.
+            char currentNameCharacter = 'A';                // The character that determines the name of the column.
+            int iteration = 1;                              // The current iteration through the uppercase letters.
+            String newColName = ("" + currentNameCharacter);// The constructed first name (should simply by "A").
 
             // Column constructor
             for (int i = 0; i < colNum; i++)
             {
-                // Instantiate a new DataColumn type
+                // Instantiate a new DataColumn with a string type to be put into the DataTable.
                 DataColumn newColumn = new DataColumn(newColName, typeof(string));
+
+                // This block simply creates the name of the next column to be added (used as it's header).
                 if (currentNameCharacter < 'Z') currentNameCharacter++;
                 else
                 {
@@ -63,12 +65,16 @@ namespace Notrosoft_Accel
                     newColName += currentNameCharacter;
                 }
                 
+                // Add the new column to the list of columns in the DataTable.
                 dataTable.Columns.Add(newColumn);
             }
             
+            // Creates a row object with the number of columns defined in the above loop.
             for (int i = 0; i < rowNum; i++)
             {
+                // Instantiate a new row.
                 DataRow newRow = dataTable.NewRow();
+                // Set the respective data of the DataTable to the dataList's. 
                 for (int j = 0; j < colNum; j++)
                 {
                     newRow[j] = dataList[i][j];
@@ -76,18 +82,20 @@ namespace Notrosoft_Accel
                 dataTable.Rows.Add(newRow);
             }
             
+            // Bind the DataGrid to the constructed dataTable through.
             Data.ItemsSource = dataTable.DefaultView;
         }
 
+        // Ensures the DataGrid is properly bound to the DataTable on window launch.
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             dataGridTable();
         }
 
+        // Handles the event of the user adding a column to the DataGrid.
         private void addColumnButton_Click(object sender, RoutedEventArgs e)
         {
-            
-            
+            // Copies all data input to the dataTable back to the dataList for permanence.
             for (int i = 0; i < rowNum; i++)
             {
                 List<string> updatedRow = new List<string>();
@@ -97,12 +105,15 @@ namespace Notrosoft_Accel
                 }
                 dataList[i] = updatedRow;
             }
+            // Inrease the column number.
             colNum++;
 
+            // Add an additional empty column to each row in the list.
             for (int i = 0; i < rowNum; i++)
             {
                 dataList[i].Add("");
             }
+            // Redo the dataTable to DataGrid binding.
             dataGridTable();
         }
     }
