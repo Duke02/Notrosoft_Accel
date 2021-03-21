b45ebf21894818c8762e95fd8b0427c7cecffe6b
Trystan May <trystanmay@gmail.com>
2021-03-21 16:54:45 -0500

    Feature/early GUI (#5)
    * Deleted the Hello World on the GUI and tried to start doing other stuff; failed; will continue soon
    
    * angle bracket gone
    
    * Added different data types.
    
    * Added documentation.
    
    * Material Design packages installed and a slight update to the GUI has been issued
    
    * Basic updates
    
    * GUI updates; DataGrid binding to DataTable complete
    
    * GUI things
    
    * Why
    
    * Why part 2
    
    * IT'S WORKING, anyways time to continue adding functionality on it or something
    
    * Comments
    
    * Comments added during the presentation lmao.
    
    Co-authored-by: Oryxson <cdf0018@uah.edu>
    Co-authored-by: CDFain <57308827+CDFain@users.noreply.github.com>

---
+++ Notrosoft_Accel/DataList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notrosoft_Accel
+{
+    public class DataList
+    {
+        public DataList(string s)
+        {
+            _value = s;
+        }
+        public string Value { get { return _value; } set { _value = value; } }
+        string _value; 
+    }
+}
--- Notrosoft_Accel/MainWindow.xaml.cs
+++ Notrosoft_Accel/MainWindow.xaml.cs
@@ -1,17 +1,120 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Windows;
+using System.Collections.ObjectModel;
 
 namespace Notrosoft_Accel
 {
     /// <summary>
-    ///     Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static int colNum = 10;  // Number of Columns present in the data structure.
+        public static int rowNum = 10;  // Number of Rows present in the data structure.
+        public static List<List<string>> dataList = new List<List<string>>();   // The data structure.
+        public static DataTable dataTable = new DataTable();  // DataTable sits between the dataList of the backend and DataGrid of the GUI.
+
         public MainWindow()
         {
+            // The List<String> representing the default column List of size colNum.
+            List<string> colConstruct = new List<string>();
+
+            for (int i = 0; i < colNum; i++)
+            {
+                colConstruct.Add("");
+            }
+            // Construct the rows as the empty column structure.
+            for (int i = 0; i < rowNum; i++)
+            {
+                dataList.Add(colConstruct);
+            }
+
             InitializeComponent();
             Console.WriteLine("Hello World!");
+        }
+
+        // Constructs a dataTable object and binds it to the DataGrid's ItemsSource.
+        void dataGridTable()
+        {
+            // Set the dataTable to a new object.
+            dataTable = new DataTable();
+
+            // Column header constructor.
+            char currentNameCharacter = 'A';                // The character that determines the name of the column.
+            int iteration = 1;                              // The current iteration through the uppercase letters.
+            String newColName = ("" + currentNameCharacter);// The constructed first name (should simply by "A").
+
+            // Column constructor
+            for (int i = 0; i < colNum; i++)
+            {
+                // Instantiate a new DataColumn with a string type to be put into the DataTable.
+                DataColumn newColumn = new DataColumn(newColName, typeof(string));
+
+                // This block simply creates the name of the next column to be added (used as it's header).
+                if (currentNameCharacter < 'Z') currentNameCharacter++;
+                else
+                {
+                    currentNameCharacter = 'A';
+                    iteration++;
+                }
+                newColName = ("");
+                for (int j = 0; j < iteration; j++)
+                {
+                    newColName += currentNameCharacter;
+                }
+                
+                // Add the new column to the list of columns in the DataTable.
+                dataTable.Columns.Add(newColumn);
+            }
+            
+            // Creates a row object with the number of columns defined in the above loop.
+            for (int i = 0; i < rowNum; i++)
+            {
+                // Instantiate a new row.
+                DataRow newRow = dataTable.NewRow();
+                // Set the respective data of the DataTable to the dataList's. 
+                for (int j = 0; j < colNum; j++)
+                {
+                    newRow[j] = dataList[i][j];
+                }                
+                dataTable.Rows.Add(newRow);
+            }
+            
+            // Bind the DataGrid to the constructed dataTable through.
+            Data.ItemsSource = dataTable.DefaultView;
+        }
+
+        // Ensures the DataGrid is properly bound to the DataTable on window launch.
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dataGridTable();
+        }
+
+        // Handles the event of the user adding a column to the DataGrid.
+        private void addColumnButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Copies all data input to the dataTable back to the dataList for permanence.
+            for (int i = 0; i < rowNum; i++)
+            {
+                List<string> updatedRow = new List<string>();
+                for (int j = 0; j < colNum; j++)
+                {
+                    updatedRow.Add(dataTable.Rows[i][j].ToString());
+                }
+                dataList[i] = updatedRow;
+            }
+            // Inrease the column number.
+            colNum++;
+
+            // Add an additional empty column to each row in the list.
+            for (int i = 0; i < rowNum; i++)
+            {
+                dataList[i].Add("");
+            }
+            // Redo the dataTable to DataGrid binding.
+            dataGridTable();
         }
     }
 }
