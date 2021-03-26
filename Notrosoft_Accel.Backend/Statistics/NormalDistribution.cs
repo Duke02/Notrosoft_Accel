dd6161c09ba6d9ae03c670bede2749cff3bb379b
Trystan May <tkmshere@yahoo.com>
2021-03-25 20:36:05 -0500

    Got normal dist and rank sum
    Signed-off-by: Trystan May <tkmshere@yahoo.com>

---
+++ Notrosoft_Accel.Backend/Statistics/NormalDistribution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Notrosoft_Accel.Infrastructure;
+
+namespace Notrosoft_Accel.Backend.Statistics
+{
+    public class NormalDistribution : IStatistic
+    {
+        public Dictionary<string, object> Operate(IEnumerable<IEnumerable<double>> values, params object[] parameters)
+        {
+            var flattenedValues = Utilities.Flatten(values).ToArray();
+
+            if (!flattenedValues.Any())
+                throw new ArgumentException("Normal Distribution Statistic requires input data.");
+
+            // Calculate the variance and the mean
+            var variance = Utilities.GetVariance(flattenedValues);
+            var mean = flattenedValues.Average();
+
+            // TODO: Return both values.
+            return new Dictionary<string, object>
+            {
+                {"mean", mean},
+                {"variance", variance}
+            };
+        }
+    }
+}
---
+++ Notrosoft_Accel.Backend/Statistics/RankSumTestStatistic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Notrosoft_Accel.Backend.Statistics
+{
+    public class RankSumTestStatistic : IStatistic
+    {
+        private const double Tolerance = .0001;
+
+        public Dictionary<string, object> Operate(IEnumerable<IEnumerable<double>> values, params object[] parameters)
+        {
+            var concreteValues = values.ToArray();
+            if (concreteValues.Length != 2)
+                throw new InvalidOperationException("Input data must be a 2D list with equal values!");
+
+            var xData = concreteValues[0].ToArray();
+            var yData = concreteValues[1].ToArray();
+
+            if (xData.Length == 0 || yData.Length == 0)
+                throw new InvalidOperationException("Input must have data in both X and Y data.");
+
+            var mannWhitneyUStat = xData.Sum(x => yData.Sum(y => InnerFunction(x, y)));
+
+            return new Dictionary<string, object>
+            {
+                {"ranksum", mannWhitneyUStat}
+            };
+        }
+
+        private double InnerFunction(double x, double y)
+        {
+            return Math.Abs(x - y) < Tolerance ? .5 : y < x ? 1 : 0;
+        }
+    }
+}
