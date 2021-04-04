737db88ab995b0333e246def31470a67d8b3e67e
Trystan May <tkmshere@yahoo.com>
2021-04-04 15:33:55 -0500

    Got Interlayer to work with the data classes.
    Signed-off-by: Trystan May <tkmshere@yahoo.com>

--- Notrosoft_Accel/Interlayer.cs
+++ Notrosoft_Accel/Interlayer.cs
@@ -6,38 +6,31 @@ namespace Notrosoft_Accel
 {
     internal class Interlayer
     {
-        private static List<List<double>> _doubles = new();
+        private static readonly List<List<double>> _doubles = new();
         private List<List<string>> _strings = new();
 
         public string doStatistics(List<List<string>> input, StatisticType[] stats, params object[] param)
         {
-            List<string> s = new List<string>();
+            var s = new List<string>();
             // Ensure List is always presented in the same manner
             if (input.Count > input[0].Count)
-            {
                 for (var i = 0; i < input.Count; i++)
                 {
                     s.Clear();
-                    for (var j = 0; j < input[0].Count; j++)
-                    { 
-                        s.Add(input[i][j]);
-                    }
+                    for (var j = 0; j < input[0].Count; j++) s.Add(input[i][j]);
                     _strings.Add(s);
-                } 
-            }
+                }
             else _strings = input;
-            
-            List<double> row = new List<double>();
+
+            var row = new List<double>();
             // Assign values for _doubles and _ints
             for (var i = 0; i < _strings.Count; i++)
             {
                 row.Clear();
-                for (var j = 0; j < _strings[0].Count; j++)
-                {
-                    row.Add(double.Parse(_strings[i][j]));
-                }
+                for (var j = 0; j < _strings[0].Count; j++) row.Add(double.Parse(_strings[i][j]));
                 _doubles.Add(row);
             }
+
             // Set the return variable for statistic operations.
             var keyValues = new Dictionary<string, object>();
             // String constructed based off the statistic performed.
@@ -45,54 +38,56 @@ namespace Notrosoft_Accel
             // String used for when a Statistical method is not implemented quite yet.
             var NaS = "Statistic method has not been added yet";
 
+            var data = new OrdinalData(_doubles);
+
             foreach (var stat in stats)
             {
                 switch (stat)
                 {
                     case StatisticType.Mean:
                         var mean = new MeanStatistic();
-                        keyValues = mean.OperateOrdinalData(_doubles, param);
+                        keyValues = mean.OperateOrdinalData(data, param);
                         keyValues.TryGetValue("mean", out var meanr);
                         returnStr += "Mean: " + meanr;
                         break;
 
                     case StatisticType.Median:
                         var median = new MedianStatistic();
-                        keyValues = median.OperateOrdinalData(_doubles, param);
+                        keyValues = median.OperateOrdinalData(data, param);
                         keyValues.TryGetValue("median", out var medr);
                         returnStr += "Median: " + medr;
                         break;
 
                     case StatisticType.Mode:
                         var mode = new ModeStatistic();
-                        keyValues = mode.OperateOrdinalData(_doubles, param);
+                        keyValues = mode.OperateOrdinalData(data, param);
                         keyValues.TryGetValue("mode", out var moder);
                         returnStr += "Mode: " + moder;
                         break;
 
                     case StatisticType.StandardDeviation:
                         var sDev = new StandardDeviationStatistic();
-                        keyValues = sDev.OperateOrdinalData(_doubles, param);
+                        keyValues = sDev.OperateOrdinalData(data, param);
                         keyValues.TryGetValue("Standard Deviation", out var SDr);
                         returnStr += "Standard Deviation: " + SDr;
                         break;
 
                     case StatisticType.Variance:
                         var variance = new VarianceStatistic();
-                        keyValues = variance.OperateOrdinalData(_doubles, param);
+                        keyValues = variance.OperateOrdinalData(data, param);
                         returnStr += "Variance:";
                         break;
 
                     case StatisticType.CoefficientOfVariance:
                         var coeffVar = new CoefficientOfVarianceStatistic();
-                        keyValues = coeffVar.OperateOrdinalData(_doubles, param);
+                        keyValues = coeffVar.OperateOrdinalData(data, param);
                         returnStr += "Coefficient of Variance:";
                         break;
 
                     case StatisticType.Percentile:
                         // Need parameter between 0 and 1 for percentile
                         var percentile = new PercentileStatistic();
-                        keyValues = percentile.OperateOrdinalData(_doubles, param);
+                        keyValues = percentile.OperateOrdinalData(data, param);
                         returnStr += "Percentile:";
                         break;
 
@@ -102,48 +97,48 @@ namespace Notrosoft_Accel
                         break;
 
                     case StatisticType.BinomialDistribution:
-                        // hypothesis, probOfSuccess, confidence (optional)
+                        // hypothesis, probOfSuccess (0-1), confidence (0-1, optional)
                         var binDis = new BinomialDistributionStatistic();
-                        keyValues = binDis.OperateOrdinalData(_doubles, param);
+                        keyValues = binDis.OperateOrdinalData(data, param);
                         returnStr += "Binomial Distribution:";
                         break;
 
                     case StatisticType.LeastSquaresLine:
                         // /outputs slope and intercept
                         var leastSqr = new LeastSquareLineStatistic();
-                        keyValues = leastSqr.OperateOrdinalData(_doubles, param);
+                        keyValues = leastSqr.OperateOrdinalData(data, param);
                         returnStr += "Least Square Line:";
                         break;
 
                     case StatisticType.ChiSquare:
                         var chiSqr = new ChiSquareStatistic();
-                        keyValues = chiSqr.OperateOrdinalData(_doubles, param);
+                        keyValues = chiSqr.OperateOrdinalData(data, param);
                         returnStr += "Chi Square:";
                         break;
 
                     case StatisticType.CorrelationCoefficient:
                         var corrCoe = new CorrelationCoefficientStatistic();
-                        keyValues = corrCoe.OperateOrdinalData(_doubles, param);
+                        keyValues = corrCoe.OperateOrdinalData(data, param);
                         returnStr += "Correlation Coefficient:";
                         break;
 
                     case StatisticType.SignTest:
                         // comparisonType, valueToCompareAgainst
                         var sign = new SignTestStatistic();
-                        keyValues = sign.OperateOrdinalData(_doubles, param);
+                        keyValues = sign.OperateOrdinalData(data, param);
                         returnStr += "Sign Test:";
                         break;
 
                     case StatisticType.RankSumTest:
-                        
+
                         var rankSum = new RankSumTestStatistic();
-                        keyValues = rankSum.OperateOrdinalData(_doubles, param);
+                        keyValues = rankSum.OperateOrdinalData(data, param);
                         returnStr += "Rank Sum:";
                         break;
 
                     case StatisticType.SpearmanRankCorrelationCoefficient:
                         var spearman = new SpearmanRankCorrelationStatistic();
-                        keyValues = spearman.OperateOrdinalData(_doubles, param);
+                        keyValues = spearman.OperateOrdinalData(data, param);
                         returnStr += "Spearman Rank Correlation:";
                         break;
 
